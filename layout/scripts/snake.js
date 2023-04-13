39797987d27476c7a96ca2b6a2e5bfcdaa50ae6b
Padurean Davide <padureand@pascal.local>
2023-04-13 12:15:37 +0200

    aggiornamenti

--- layout/scripts/snake.js
+++ layout/scripts/snake.js
@@ -45,12 +45,15 @@ $("#music").click(function(){
       }
 });
 
+//-------------------------------------------------------------------------------------------------------------------
 //------------------------FUNZIONI E VARIABILI NECESSARIE AL CORRETTO FUNZIONAMENTO DEL GIOCO------------------------
 const campoGioco = document.getElementById("playPlace");
-var varX, varY, serpX = 5, serpY = 10;
+var varX, varY, serpX = 23, serpY = 10;
 var direzX = 0, direzY = 0;
-var codaSerpente = [];
+let codaSerpente = [];
+var controlloIntervallo;
 
+//con questa funzione posizioniamo il cibo in modo randomico
 function posizioneCibo()
 {
     ciboX = Math.floor(Math.random() * 30) + 1;
@@ -59,20 +62,47 @@ function posizioneCibo()
 
 //funzione che crea il cibo del serpente
 function posizioneCiboSerpente(){
-    
-    serpX += direzX;
-    serpY += direzY;
+ 
+    let posizioni = `<div class="serpentFood" style="grid-area: ${ciboY} / ${ciboX}"></div>`;    
 
-    let posizioni = `<div class="serpentFood" style="grid-area: ${ciboX} / ${ciboY}"></div>`;
+    //controllo se il serprente ha colpito uno dei muri
+    if(serpX<= 0 || serpX >= 30 || serpY <= 0 || serpY >= 30)
+    {
+        clearInterval(controlloIntervallo);
+        alert("gioco finito!!!");
+    }
+    else
+    {
+        //aggiornamento della posizione del serprnte 
+        serpX += direzX;
+        serpY += direzY;
+    }
 
+    //controllo se il serpente sta sulla casella del cibo
     if(serpX === ciboX && serpY === ciboY)
     {
         posizioneCibo(); //creo un nuovo cibo in modo randomico sul campo
         codaSerpente.push([ciboX, ciboY]); //salvo la coda del serpente
+        console.log("preso il cibo");
     }
 
-    posizioni += `<div class="serpentHead" style="grid-area: ${serpX} / ${serpY}"></div>`;
+    for(var i = codaSerpente.length - 1; i>0; i--)
+    {
+        codaSerpente[i] = codaSerpente[i - 1];
+    }
 
+    //posizioniamo il serpente nella casella voluta
+    codaSerpente[0] = [serpX, serpY];
+
+    
+
+    for(var i=0; i< codaSerpente.length; i++)
+    {
+        posizioni += `<div class="serpentHead" style="grid-area: ${codaSerpente[i][1]} / ${codaSerpente[i][0]}"></div>`;
+    }
+
+
+    //creazione del campo di gioco
     campoGioco.innerHTML = posizioni;    
 }
 
@@ -80,9 +110,7 @@ function posizioneCiboSerpente(){
 posizioneCibo();
 
 //posiziono il serpente e il cibo sull'area di gioco
-setInterval(posizioneCiboSerpente, 200);
-
-
+controlloIntervallo = setInterval(posizioneCiboSerpente, 100);
 
 //al click sul tasto il serpente si muoverà nella direzione voluta
 document.onkeydown = muoviSerpente;
@@ -91,37 +119,27 @@ document.onkeydown = muoviSerpente;
     {
         if(e.key == "ArrowUp")
         {
-            direzX = -1;
-            direzY = 0;
-        }
-
-        if(e.key == "ArrowDown")
-        {
-            direzX = 1;
-            direzY = 0;
-        }
-
-        if(e.key == "ArrowLeft")
-        {
             direzX = 0;
             direzY = -1;
         }
 
-        if(e.key == "ArrowRight")
+        if(e.key == "ArrowDown")
         {
             direzX = 0;
             direzY = 1;
         }
+
+        if(e.key == "ArrowLeft")
+        {
+            direzX = -1;
+            direzY = 0;
+        }
+
+        if(e.key == "ArrowRight")
+        {
+            direzX = 1;
+            direzY = 0;
+        }
         //eseguiamo la funzione per aggiornare la posizone del nostro serpente
         posizioneCiboSerpente();
     }
-
-
-
-
-
-
-
-
-
-
