7b9fe9845be9f1d70feb80792542e0f461128a13
Simone <binisimone04@gmail.com>
2023-05-14 18:14:41 +0200

    modifiche snake

--- layout/scripts/snake.js
+++ layout/scripts/snake.js
@@ -1,50 +1,3 @@
-
-
-//------------------------FUNZIONI E VARIABILI NECESSARIE------------------------
-
-//dichiarazione variabili
-var img = document.createElement("img");
-var song = true;
-let punteggio = 0;
-var audioElement = document.createElement('audio');
-
-//$(".game").hide();
-setInterval(start, 3000);
-
-function start(){
-    $(".game").show();
-    //$("div:first").hide();
-}
-
-//Funzione per mettere in pausa il gioco
-$("#state").click(function(){
-    alert("Gioco in pausa");
-});
-
-//Funzione per ricaricare il gioco
-$("#restart").click(function(){
-    if(confirm("Riavviare la partita?")) window.location.reload();
-});
-
-//Funzione per far partire la musica all'avvio del gioco
-window.addEventListener("load", (event) => {
-    audioElement.setAttribute('src', 'sounds/tetris.mp3');
-    audioElement.play();
-});
-
-//Funzione per gestire la musica
-audioElement.setAttribute('src', 'sounds/tetris.mp3');
-$("#music").click(function(){
-    if(song) {
-        audioElement.pause();
-        song = false;
-      }
-      else {
-        audioElement.play();
-        song = true;
-      }
-});
-
 //---------------------------------------------------------------------------------------
 //-----------------------------COSTANTI E VARIABILI RIGUARDANTE IL GIOCO
 let inputDir = {x: 0, y: 0}; 
@@ -53,8 +6,36 @@ let puntAttuale = 0;
 let lastPaintTime = 0;
 var controlloIntervallo;
 var contatoreXControllo = 0;
+var puntMigliore=0;
+var numeroXlocal = 0;
 
+const suonoCibo = document.createElement('audio');
+const gameOver = document.createElement('audio');
+const movimentoSerpente = document.createElement('audio');
+const musicaBackground = document.createElement('audio');
+var img = document.createElement("img");
+var song = true;
+let punteggio = 0;
+var audioElement = document.createElement('audio');
+
+$(".game").hide();
+$(".gioco").hide();
+$("#board").hide();
+$(".visuaPunteggio").hide();
+setInterval(start, 2000);
+function start(){
+    $(".game").show();
+    $(".gioco").show();
+    $("#board").show();
+    $(".visuaPunteggio").show();
+    $("div:first").hide();
+}
+//Funzione per cambiare tra la loading bar e far iniziare il gioco
+
+//---------------------------------------------------------------------------------------
+//posizioniamo sia il cibo che il serpente
 food = {x: 6, y: 7};
+
 let snakeArr = [
     {x: 13, y: 15}
 ];
@@ -62,23 +43,15 @@ let snakeArr = [
 //---------------------------------------------------------------------------------------
 //-----------------------------FUNZIONI RIGUARDANTI IL GIOCO-----------------------------
 
-
-
-/*
-function main(ctime) {
-    window.requestAnimationFrame(main);
-    // console.log(ctime)
-    if((ctime - lastPaintTime)/1000 < 1/speed){
-        return;
-    }
-    lastPaintTime = ctime;
-    gameEngine();
-}
-*/
+//controllo se il serpente ha colpito sestesso o uno dei bordi
 function isCollide(snake) {
     //se ti colpisci dasolo
     for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === ((snake[0].x)+inputDir.x) && snake[i].y === ((snake[0].y)+inputDir.y)){
+        if(snake[i].x === ((snake[0].x)+inputDir.x) && snake[i].y === ((snake[0].y)+inputDir.y)){             
+            //musica di qunado perdiamo
+            gameOver.setAttribute('src', 'sounds/snake/gameover.mp3');
+            gameOver.play();
+            //aggiornamento del punteggio
             document.getElementById("bestPunt").innerHTML = puntAttuale;
             document.getElementById("puntNow").innerHTML = 0;
             puntAttuale = 0;
@@ -87,15 +60,21 @@ function isCollide(snake) {
     }
     //se vai contro ai bordi della board
     if(((snake[0].x)+inputDir.x) > 20 || ((snake[0].x)+inputDir.x) <=0 || ((snake[0].y)+inputDir.y) > 20 || ((snake[0].y)+inputDir.y) <=0){
+        //musica di qunado perdiamo
+        gameOver.setAttribute('src', 'sounds/snake/gameover.mp3');
+        movimentoSerpente.pause();
+        musicaBackground.pause();
+        gameOver.play();
+        //aggiornamento del punteggio
         document.getElementById("bestPunt").innerHTML = puntAttuale;
         document.getElementById("puntNow").innerHTML = 0;
         puntAttuale = 0;
         return true;
     }
-        
     return false;
 }
 
+//-------------------------------------------------------------------------
 //funzione che mi da le coordinate della posizione successiva del cibo
 function randomPosizioneCibo()
 {
@@ -103,21 +82,51 @@ function randomPosizioneCibo()
     food.y = Math.floor(Math.random() * 20) + 1
 }
 
+//funzione che mostra il div del messaggio
+function mostraMessaggio()
+{
+    var mess = document.getElementById("messaggio");
+    mess.style.display = 'block';
+}
 
-function gameEngine(){
-    // Part 1: Updating the snake array & Food
-    
+function nascondiMessaggio()
+{
+    var mess = document.getElementById("messaggio");
+    mess.style.display = 'none';
+}
+
+function premiINVIO()
+{
+    document.addEventListener('keypress', function(event) {
+        // Controlla se il tasto premuto è il tasto Invio
+        if(event.key === 'Enter') {
+            nascondiMessaggio();
+            snakeArr = [{x: 13, y: 15}]; //riposiziono il serpente nella casella 13, 15
+            score = 0;
+            return true;
+        }
+        return false;
+      });
+}
+
+//---------------------------------------------------------------------------------------------
+//--------------------FUNZIONE CHE REGOLA E GESTISCE TUTTO IL GIOCO----------------------------
+function gameEngine(){   
+    //messaggio che appare se il serpente si è copito dasolo o ha sbattuto contro un bordo
     if(isCollide(snakeArr)){
-        inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
-        snakeArr = [{x: 13, y: 15}];
-        score = 0; 
+        inputDir = {x: 0, y: 0}; //aggiorno a 0 sia le direzioni x e y del serpente così non si muove        
+        mostraMessaggio(); //faccio apparire il div che mi chiede se voglio continuare a giocare
+
+        if(premiINVIO()===false) premiINVIO(); //il giocatore deve premere perforza il tasto INVIO per continuare la partita
+
+        snakeArr = [{x: 13, y: 15}]; //riposiziono il serpente nella casella 13, 15
+        score = 0;
     }
     
     //controllo se si ha mangiato un frutto
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
-        //$(".food").css("background-image", 'url(layout/styles/headU.png)');
-        
+        suonoCibo.setAttribute('src', 'sounds/snake/food.mp3');
+        suonoCibo.play();
         puntAttuale++; //incremento del nostro punteggio attuale
         document.getElementById("puntNow").innerHTML = puntAttuale; //visualizzazione del punteggio attuale
         
@@ -160,52 +169,100 @@ function gameEngine(){
     
 }
 
-//window.requestAnimationFrame(main);
 //premendo uno dei 4 tasti il serpente si muoverà nella direzione voluta, non potr
 const rotazione = document.getElementsByClassName("head");
 
 document.onkeydown = muoviSerpente;
 
-    function muoviSerpente(e)
+function muoviSerpente(e)
+{
+    if(e.key == "ArrowUp" && inputDir.y != 1)
     {
-        if(e.key == "ArrowUp" && inputDir.y != 1)
-        {
-            //$(".head").css("background-image", 'url(images/snake/headU.png)');
-            inputDir.x = 0;
-            inputDir.y = -1;
-
-        }
-
-        if(e.key == "ArrowDown" && inputDir.y != -1)
-        {
-            //$(".head").css("background-image", 'url(images/snake/headD.png)');
-            inputDir.x = 0;
-            inputDir.y = 1;
-        }
-
-        if(e.key == "ArrowLeft" && inputDir.x != 1)
-        {
-            //$(".head").css("background-image", 'url(images/snake/headL.png)');
-            inputDir.x = -1;
-            inputDir.y = 0;
-        }
-
-        if(e.key == "ArrowRight" && inputDir.x != -1)
-        {
-            //$(".head").css("background-image", 'url(images/snake/headR.png)');
-            inputDir.x = 1;
-            inputDir.y = 0;
-        }
-        //eseguiamo la funzione per aggiornare la posizone del nostro serpente
-        gameEngine();
+        inputDir.x = 0;
+        inputDir.y = -1;
     }
 
-    controlloIntervallo = setInterval(gameEngine, 1500/10);
+    if(e.key == "ArrowDown" && inputDir.y != -1)
+    {
+        inputDir.x = 0;
+        inputDir.y = 1;
+    }
 
+    if(e.key == "ArrowLeft" && inputDir.x != 1)
+    {
+        inputDir.x = -1;
+        inputDir.y = 0;
+    }
 
+    if(e.key == "ArrowRight" && inputDir.x != -1)
+    {
+        inputDir.x = 1;
+        inputDir.y = 0;
+    }
 
+    gameEngine();
+}
 
+controlloIntervallo = setInterval(gameEngine, 1500/10);
 
+$("#home").click(function(){
+    musicaBackground.pause();
+    if(confirm("Tornare alla pagina principale?")) {
+        $(window).prop("location", "index.html");
+    }
+    else musicaBackground.play(); 
+});
+//Funzione per tornare alla pagina principale
 
+$("#music").click(function(){
+    if(song) {
+        musicaBackground.pause();
+        song = false;
+      }
+      else {
+        musicaBackground.play();
+        song = true;
+      }
+});
+//Funzione per mettere in pausa e riprendere la musica
 
+document.addEventListener("keydown", function(event) {
+    if(event.key === "+"){
+        musicaBackground.volume += 0.1;
+    }
+    else if(event.key === "-"){
+        musicaBackground.volume -= 0.1;
+    }
+});
+//Funzione per aumentare e diminuire il volume della musica
 
+window.onblur = function() {
+    musicaBackground.pause();
+    movimentoSerpente.setAttribute('src', '');
+}
+//Funzione per mettere in pausa la musica quando si cambia scheda
+    
+window.onfocus = function() {
+    musicaBackground.play();
+    movimentoSerpente.setAttribute('src', 'sounds/snake/movimentoSerpente.mp3');
+}
+//Funzione per riprodurre la musica quando si ritorna alla scheda
+
+window.addEventListener("load", (event) => {
+    setTimeout(function() {
+        musicaBackground.setAttribute('src', 'sounds/snake/SoundTrack.mp3');
+        musicaBackground.volume = 0.1;
+        musicaBackground.play();
+    }, 2000);
+});
+//Funzione per far partire la musica di sottofondo
+
+//Funzione per mettere in pausa il gioco
+$("#state").click(function(){
+    alert("Gioco in pausa");
+});
+
+//Funzione per ricaricare il gioco
+$("#restart").click(function(){
+    if(confirm("Riavviare la partita?")) window.location.reload();
+});
